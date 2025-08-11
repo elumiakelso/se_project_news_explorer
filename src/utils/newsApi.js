4d3b4d6bcdf4419600b578cd2f9b0421b7bc4069
Emily Kelso <elumiakelso@gmail.com>
2025-08-10 22:49:30 -0700

    connect to API, initial search functions

---
+++ src/utils/newsApi.js
@@ -0,0 +1,11 @@
+import { APIkey } from "./constants";
+
+import newsApiBaseUrl from "./constants";
+
+export async function fetchNewsArticles(query) {
+  const url = `${newsApiBaseUrl}?q=${encodeURIComponent(query)}&apiKey=${APIkey}&pageSize=100&language=en`;
+
+  const response = await fetch(url);
+  if (!response.ok) throw new Error('Network response was not ok');
+  return response.json();
+}
